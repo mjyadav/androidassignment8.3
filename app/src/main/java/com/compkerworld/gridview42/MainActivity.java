9870033f3fbc26aa8bdb642e6ebb27ddd88c3167
mjyadav <mrityunjay.yadav0007@gmail.com>
2017-07-02 20:13:54 +0530

    ass
    ass

---
+++ app/src/main/java/com/compkerworld/gridview42/MainActivity.java
@@ -0,0 +1,27 @@
+package com.compkerworld.gridview42;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);//find id
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+    }
+}
